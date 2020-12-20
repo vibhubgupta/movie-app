fcc8a3c67a68a1842bb497904f26f7f9903714e5
thewittybrown <vibhubg844@gmail.com>
2020-12-21 01:18:51 +0530

    added unfav movie option and changing tabs

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,10 @@
 
 //action types
 export const ADD_MOVIES = 'ADD_MOVIES';
-export const ADD_FAVOURITE = 'ADD_FAVOURITE';
+export const ADD_TO_FAVOURITE = 'ADD_TO_FAVOURITE';
+export const REMOVE_FROM_FAVOURITES = 'REMOVE_FROM_FAVOURITES';
+export const SET_SHOW_FAVOURITES = 'SET_SHOW_FAVOURITES';
+
 
 //action creators
 export function addMovies (movies) {
@@ -17,7 +20,19 @@ export function addMovies (movies) {
 }
 export function addFavourite (movie) {
     return {
-        type: ADD_FAVOURITE,
+        type: ADD_TO_FAVOURITE,
         movie
+    }
+}
+export function removeFromFavourites(movie) {
+    return {
+        type: REMOVE_FROM_FAVOURITES,
+        movie
+    };
+}
+export function setShowFavourites(val) {
+    return {
+        type: SET_SHOW_FAVOURITES,
+        val
     }
 }
