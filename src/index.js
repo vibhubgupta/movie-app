26723b5e23b25e41abc564ab3f5965516eb41b49
thewittybrown <vibhubg844@gmail.com>
2020-12-20 22:18:22 +0530

    added store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+
+const store = createStore(movies);
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
